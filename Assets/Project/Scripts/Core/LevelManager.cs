4184451b73c466022ad66fbf878176b67cf7d744
javier-ll <llanojavier97@gmail.com>
2026-02-17 23:44:14 -0300

    Feat: Menu principal inmersivo, logic de escenas y feedback de audio UI

---
+++ Assets/Project/Scripts/Core/LevelManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Necesario para cambiar escenas
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadResults()
+    {
+        SceneManager.LoadScene("Results");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+    }
+}
